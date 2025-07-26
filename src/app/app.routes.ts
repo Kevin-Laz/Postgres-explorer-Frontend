7c3c201e9cd38e3e7d270b9ecb21e9b94259b23f
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2025-07-26 00:24:47 -0500

    Configuración incial del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './features/start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StartComponent },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.dashboardRoute)
+  },
+  { path: '**', redirectTo: '' }
+];
