72c06aae58d846145f2f67ee4cd36c3f26625ce5
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2025-07-28 02:20:30 -0500

    Implementación inicial de la lógica del formulario

--- src/app/features/start/start.component.ts
+++ src/app/features/start/start.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
 export class StartComponent {
+  dbForm: FormGroup;
 
+  constructor(private fb:FormBuilder){
+    this.dbForm = this.fb.group({
+      connectionString: ['', [Validators.required, Validators.pattern(/^postgresql:\/\/.+$/)]]
+    })
+  }
+
+  onSubmit(){
+    if(this.dbForm.invalid){
+      this.dbForm.markAllAsTouched();
+      console.log("Invalido");
+      return;
+    }
+    const connection = this.dbForm.value.connectionString;
+    console.log("Iniciando conexion...")
+  }
 }
