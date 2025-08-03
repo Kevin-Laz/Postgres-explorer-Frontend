e525089879d41484cf811ef065c20d28f85ec899
Kevin Jhorman Liñan Ariza <kevin-laz@outlook.com>
2025-08-03 10:01:06 -0500

    Implementación de la lógica de herramientas

---
+++ src/app/data/interface/tool.interface.ts
@@ -0,0 +1,76 @@
+export interface ToolOption{
+  name: string;
+  icon: string;
+  action: string;
+}
+
+export interface Tool{
+  name: string,
+  label: string,
+  icon: string,
+  options: ToolOption[];
+}
+
+export const TOOLS: Tool[] = [
+    {
+      name: 'tabla',
+      label: 'Tabla',
+      icon: 'icon-table',
+      options: [
+        { name: 'Crear', icon: 'create-icon', action: 'createTable' },
+        { name: 'Editar', icon: 'edit-icon', action: 'editTable' },
+        { name: 'Eliminar', icon: 'delete-icon', action: 'deleteTable'},
+        { name: 'Duplicar', icon: 'duplicate-icon', action: 'duplicateTable'},
+        { name: 'Detalles', icon: 'details-icon', action: 'viewTableDetails'}
+      ]
+    },
+
+    {
+      name: 'Columna',
+      label: 'Columna',
+      icon: 'icon-column',
+      options: [
+        { name: 'Crear', icon: 'create-icon', action: 'createColumn' },
+        { name: 'Editar', icon: 'edit-icon', action: 'editColumn' },
+        { name: 'Eliminar', icon: 'delete-icon', action: 'deleteColumn'},
+        { name: 'Reordenar', icon: 'reorder-icon', action: 'reorderColumn'},
+        { name: 'Detalles', icon: 'details-icon', action: 'viewColumnDetails'}
+      ]
+    },
+
+    {
+      name: 'relacion',
+      label: 'Relación',
+      icon: 'icon-relationship',
+      options: [
+        { name: 'Crear', icon: 'create-icon', action: 'createRelation' },
+        { name: 'Editar', icon: 'edit-icon', action: 'editRelation' },
+        { name: 'Eliminar', icon: 'delete-icon', action: 'deleteRelation'},
+        { name: 'Detalles', icon: 'details-icon', action: 'viewColumnDetails'}
+      ]
+    },
+
+    {
+      name: 'clave',
+      label: 'Clave',
+      icon: 'icon-primary',
+      options: [
+        { name: 'Crear', icon: 'create-icon', action: 'createPrimaryKey' },
+        { name: 'Editar', icon: 'edit-icon', action: 'editPrimaryKey' },
+        { name: 'Eliminar', icon: 'delete-icon', action: 'deletePrimaryKey'},
+        { name: 'Detalles', icon: 'details-icon', action: 'viewPrimaryKeyDetails'},
+      ]
+    },
+
+    {
+      name: 'restriccion',
+      label: 'Restricción',
+      icon: 'icon-constraint',
+      options: [
+        { name: 'Crear', icon: 'create-icon', action: 'createConstraint' },
+        { name: 'Editar', icon: 'edit-icon', action: 'editConstraint' },
+        { name: 'Eliminar', icon: 'delete-icon', action: 'deleteConstraint'},
+        { name: 'Detalles', icon: 'details-icon', action: 'viewConstraintDetails'},
+      ]
+    },
+  ]
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Tool, ToolOption, TOOLS } from '../../data/interface/tool.interface';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,4 +12,42 @@ import { FormsModule } from '@angular/forms';
 export class SidebarComponent {
   activeTab = 'sql';
   sqlQuery = '';
+
+  toolsDB = TOOLS;
+  options_active: ToolOption[] | null = null;
+  tool_selected: string | null = null;
+  hovered_tool: string | null = null;
+  isToolFixed = false;
+
+  isOptionFixed = false;
+
+
+  previewTool(tool: Tool): void {
+    if (!this.isToolFixed) {
+      this.hovered_tool = tool.name;
+      this.options_active = tool.options;
+    }
+  }
+
+  selectTool(tool: Tool): void {
+    if (this.tool_selected === tool.name && this.isToolFixed) {
+      // Deseleccionar si ya está seleccionada
+      this.isToolFixed = false;
+      this.tool_selected = null;
+      this.options_active = null;
+    } else {
+      // Seleccionar nueva herramienta
+      this.isToolFixed = true;
+      this.tool_selected = tool.name;
+      this.options_active = tool.options;
+    }
+  }
+
+  clearOptions(): void {
+    if (!this.isToolFixed) {
+      this.hovered_tool = null;
+      this.options_active = null;
+    }
+  }
+
 }
